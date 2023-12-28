83e2c2b1c03a02a0d7f209b0b55f68ea5d091038
Dongli99 <dongliliu0@gmail.com>
2023-12-27 22:16:17 -0800

    used global fetchProfile func in UserContext

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -1,20 +1,12 @@
 import { createContext, useEffect, useState } from "react";
-import { axiosInstance } from "../services/axios";
+import { fetchProfile } from "../utils/fetchProfile";
 
 export const UserContext = createContext({});
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const fetchProfile = async () => {
-    try {
-      const response = await axiosInstance.get("/api/user/profile");
-      setUser(response.data);
-    } catch (error) {
-      console.error("Error fetching user profile:", error);
-    }
-  };
   useEffect(() => {
-    fetchProfile();
+    fetchProfile(setUser);
   }, []);
 
   return (
