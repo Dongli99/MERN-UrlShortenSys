45157b447d3173d1f1e320eaea4de32ec8f8c759
Dongli99 <dongliliu0@gmail.com>
2023-12-30 21:13:56 -0800

    created deploy logic

--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ import config from "./config/config.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import urlPairRouter from "./routes/urlPair.routes.js";
-import CurrAlias from "./models/CurrAlias.model.js";
 import UrlPairController from "./controllers/urlPair.controller.js";
-/*--delete after moving to controller--*/
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
@@ -24,12 +25,24 @@ app.use(express.json());
 app.use(CookieParser());
 
 connectDB();
+console.log(__dirname);
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/uss", urlPairRouter);
 app.get("/:alias", UrlPairController.redirectToOrigin);
 app.get("/test", UrlPairController.redirectToOrigin);
+
+if (config.nodeEnv === "production") {
+  const distPath = path.join(__dirname, "../client/dist");
+  app.use(express.static(distPath));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(distPath, "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Not on the production mode."));
+}
 
 app.listen(config.port, () => {
   console.log("Server is running on " + config.port);
