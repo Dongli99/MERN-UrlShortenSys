beb65a6658d622538dd080443ef7c40932152bf9
Dongli99 <dongliliu0@gmail.com>
2024-01-04 21:14:59 -0800

    wrote recursive function to avoid autogenerated conflict alias

--- server/controllers/currAlias.controller.js
+++ server/controllers/currAlias.controller.js
@@ -1,6 +1,7 @@
 import CurrAlias from "../models/currAlias.model.js";
 import config from "../config/config.js";
 import AliasService from "../services/AliasService.js";
+import { ExceptionRoutes } from "../routes/exceptionRoutes.js";
 
 class CurrAliasController {
   static async updateCurrAlias() {
@@ -8,8 +9,12 @@ class CurrAliasController {
       var currAlias = await CurrAlias.findById(config.currAliasId);
       currAlias.count += 1;
       currAlias.alias = AliasService.encode(currAlias.count);
-      await currAlias.save();
-      return currAlias.alias;
+      if (ExceptionRoutes.includes(currAlias.alias)) {
+        CurrAliasController.updateCurrAlias();
+      } else {
+        await currAlias.save();
+        return currAlias.alias;
+      }
     } catch (err) {
       throw err;
     }
